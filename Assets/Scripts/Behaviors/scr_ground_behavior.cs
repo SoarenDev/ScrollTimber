b454ea4b0a48d9bd1265cebc61079960055364c2
ETU\t.ledieu <t.ledieu@rubika-edu.com>
2019-03-04 17:53:18 +0100

    //

---
+++ Assets/Scripts/Behaviors/scr_ground_behavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_ground_behavior : MonoBehaviour
+{
+
+// = = = [ VARIABLES DEFINITION ] = = =
+
+
+       
+// = = =
+
+// = = = [ MONOBEHAVIOR METHODS ] = = =
+
+
+
+// = = =
+
+// = = = [ CLASS METHODS ] = = =
+
+
+
+// = = =
+
+}
