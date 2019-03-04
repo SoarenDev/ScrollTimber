59250f10c0c316c9aea16b3da677d598bba3fcef
ETU\t.ledieu <t.ledieu@rubika-edu.com>
2019-03-04 15:12:50 +0100

    GameManager setup

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+public static GameManager instance;
+
+// = = = [ VARIABLES DEFINITION ] = = =
+
+
+       
+// = = =
+
+// = = = [ MONOBEHAVIOR METHODS ] = = =
+
+    void Awake()
+    {
+        if ( instance == null )
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+// = = =
+
+// = = = [ CLASS METHODS ] = = =
+
+
+
+// = = =
+
+}
