b975c7481ea3182752a43329596308d727b5985c
ETU\t.ledieu <t.ledieu@rubika-edu.com>
2019-03-05 15:16:02 +0100

    tree object created with auto generation of "needed cut data" vector

--- Assets/Scripts/Behaviors/scr_tree_behavior.cs
+++ Assets/Scripts/Behaviors/scr_tree_behavior.cs
@@ -8,10 +8,10 @@ public class scr_tree_behavior : MonoBehaviour
 // = = = [ VARIABLES DEFINITION ] = = =
 
 [Space(10)][Header("Runtime")]
-    public  float                   weak_point_position                 ;           // actual weak point position in tree's height ratio (runtime generated)
+    public  Vector3                 weak_point_direction                ;           // actual weak point direction, used to compare with director vector
 
 [Space(10)][Header("Gameplay")]
-    public  float                   weak_point_spawnRange               = 0.50f;           // range in which the weak point can spawn (ratio of the tree's height)
+    public  float                   weak_point_direction_max           = 0.50f;     // maxmimal angular ratio that the cut can reach (negative and positive)
   
 // = = =
 
@@ -41,9 +41,28 @@ public class scr_tree_behavior : MonoBehaviour
     /// </summary>
     public void InitializeWeakPoint()
     {
-        weak_point_position = Random.Range(0, weak_point_spawnRange);
+        weak_point_direction = GenerateCutDirection();
+        // Debug.Log(weak_point_direction);
+        // Debug.DrawLine(gameObject.transform.position, gameObject.transform.position+weak_point_direction, Color.yellow, 10.00f);
         return;
     }
+
+    /// <summary>
+	/// Returns a normalized vector that indicates a direction.
+	/// </summary>
+	public Vector3 GenerateCutDirection()
+	{
+		Vector3 direction_vector = new Vector3(0,0,0);
+
+		// normalize combined axis inputs' value
+		float normalize_factor;  // factor used to normalise the vector
+		direction_vector.x = 0.50f;
+		direction_vector.y = Random.Range(-weak_point_direction_max, weak_point_direction_max);
+		normalize_factor = Mathf.Abs(direction_vector.x) + Mathf.Abs(direction_vector.y);
+				
+		// return normalized direction vector
+		return (direction_vector / normalize_factor);
+	}
 
 // = = =
 
