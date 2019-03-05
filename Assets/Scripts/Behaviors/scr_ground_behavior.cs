73af1cb89421adcf90bc77e955d8d97de4639d60
ETU\t.ledieu <t.ledieu@rubika-edu.com>
2019-03-05 11:04:34 +0100

    base scrolling working

--- Assets/Scripts/Behaviors/scr_ground_behavior.cs
+++ Assets/Scripts/Behaviors/scr_ground_behavior.cs
@@ -7,19 +7,37 @@ public class scr_ground_behavior : MonoBehaviour
 
 // = = = [ VARIABLES DEFINITION ] = = =
 
-
+[Space(10)][Header("Gameplay")]
+    public  float       scroll_speed            ;
        
 // = = =
 
 // = = = [ MONOBEHAVIOR METHODS ] = = =
 
-
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        // parent cylinder scrolling
+        if (ScrollingManager.instance.is_scrolling_disabled == false)
+        {
+            ScrollCylinder(scroll_speed*Time.deltaTime);
+        }
+    }
 
 // = = =
 
 // = = = [ CLASS METHODS ] = = =
 
-
+    /// <summary>
+    /// Rotates the parent cylinder gameobject to represent the scrolling of the ground.
+    /// </summary>
+    public void ScrollCylinder(float value)
+    {
+        ScrollingManager.instance.parent_scroll_cylinder_ref.transform.Rotate(-value, 0, 0);
+        return;
+    }
 
 // = = =
 
--- Assets/Scripts/Managers/ScrollingManager.cs
+++ Assets/Scripts/Managers/ScrollingManager.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class ScrollingManager : MonoBehaviour
 {
-
+    public static ScrollingManager instance;
+    
 // = = = [ VARIABLES DEFINITION ] = = =
 
+[Space(10)][Header("Runtime")]
+    public bool             is_scrolling_disabled           = false;
 
-       
+[Space(10)][Header("References")]
+    public GameObject       parent_scroll_cylinder_ref      ;
+
 // = = =
 
 // = = = [ MONOBEHAVIOR METHODS ] = = =
 
-
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
 
 // = = =
 
