82b58d9aa3d769a91a41e67c12cf5b8757d81c1a
ETU\a.coteyangting <s-a-f-e-r@outlook.fr>
2019-03-06 19:53:13 +0100

    Adrien joined the party Post process stack integrated Screen shake and juice

---
+++ Assets/Scripts/CutFx.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class CutFx : MonoBehaviour
+{
+
+    PostProcessVolume CutProcessVolume;
+    public AnimationCurve ProcessCurve;
+    public float curveLength;
+    public float slowMoFactor;
+    public float maxSlowMoLength;
+    float time;
+    float initialFixedDeltaTime;
+
+    void Start()
+    {
+        CutProcessVolume = gameObject.GetComponent<PostProcessVolume>();
+        initialFixedDeltaTime = Time.fixedDeltaTime;
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            //StartCoroutine(OnTreeCut());
+            Metadesc.CameraShake.ShakeManager.I.AddShake("CutShake");
+        }
+    }
+
+    public IEnumerator OnTreeCut()
+    {
+        //début du slow mo
+        Time.timeScale = slowMoFactor;
+        Time.fixedDeltaTime = initialFixedDeltaTime * Time.timeScale;
+        while (time <= curveLength)
+        {
+            //update du weight du post process
+            CutProcessVolume.weight = ProcessCurve.Evaluate(time);
+            time += Time.deltaTime;
+            if (time >= maxSlowMoLength)
+            {
+                //retour smooth du temps à la normale
+                Time.timeScale = slowMoFactor + (1 - slowMoFactor) * ((curveLength - time) / (curveLength - maxSlowMoLength));
+                Time.fixedDeltaTime = initialFixedDeltaTime * Time.timeScale;
+            }
+            yield return null;
+        }
+        Time.timeScale = 1f;
+        time = 0f;
+        CutProcessVolume.weight = 0;
+        yield break;
+    }
+}
