c7e632558be966df4cbbfcb767039f69054cb156
ETU\a.coteyangting <s-a-f-e-r@outlook.fr>
2019-03-07 20:04:44 +0100

    Updated mesh separation script (to test)

--- Assets/Scripts/MeshSeparation.cs
+++ Assets/Scripts/MeshSeparation.cs
@@ -7,8 +7,10 @@ public class MeshSeparation : MonoBehaviour
 {
     public float slashWidth = 0.01f;
     private GameObject scrollCylinderParent;
-    public GameObject upperObjectToSpawn;
-    public GameObject lowerObjectToSpawn;
+    [Header("Please order meshes and corresponding prefabs in the same way")]
+    public List<Mesh> possibleMeshes;
+    public List<GameObject> correspondingUpperTrees;
+    public List<GameObject> correspondingLowerTrees;
     GameObject upperObjectInstance;
     GameObject lowerObjectInstance;
     Mesh upperMesh;
@@ -20,6 +22,7 @@ public class MeshSeparation : MonoBehaviour
     void Start()
     {
         InitializeVariables();
+        OnCut(45f, 0.1f);
     }
 
     void InitializeVariables()
@@ -30,50 +33,58 @@ public class MeshSeparation : MonoBehaviour
 
     public void OnCut(float angle, float height)
     {
-        upperObjectInstance = Instantiate(upperObjectToSpawn, transform.position, transform.rotation, scrollCylinderParent.transform);
-        upperMesh = upperObjectInstance.GetComponent<MeshFilter>().mesh;
-        verticesToMove = upperMesh.vertices.ToList<Vector3>();
-
-        for (int i = 0; i < upperMesh.vertices.Count(); i++)
+        for (int j = 0; j < possibleMeshes.Count(); j++)
         {
-            if (upperMesh.vertices[i].z < height + Mathf.Tan(angle * (Mathf.PI / 180)) * verticesToMove[i].y + slashWidth)
+            if (gameObject.GetComponent<MeshFilter>().mesh.name.Contains(possibleMeshes[j].name))
             {
-                verticesIndexes.Add(i);
+                upperObjectInstance = Instantiate(correspondingUpperTrees[j], transform.position, transform.rotation, scrollCylinderParent.transform);
+                upperObjectInstance.transform.localScale = transform.localScale;
+                upperMesh = upperObjectInstance.GetComponent<MeshFilter>().mesh;
+                verticesToMove = upperMesh.vertices.ToList<Vector3>();
+
+                for (int i = 0; i < upperMesh.vertices.Count(); i++)
+                {
+                    if (upperMesh.vertices[i].z < height + Mathf.Tan(angle * (Mathf.PI / 180)) * verticesToMove[i].y + slashWidth)
+                    {
+                        verticesIndexes.Add(i);
+                    }
+                }
+
+                for (int i = 0; i < verticesIndexes.Count(); i++)
+                {
+                    verticesToMove[verticesIndexes[i]] = new Vector3(verticesToMove[verticesIndexes[i]].x, verticesToMove[verticesIndexes[i]].y, height + Mathf.Tan(angle * (Mathf.PI / 180)) * verticesToMove[verticesIndexes[i]].y + slashWidth);
+                }
+                upperMesh.vertices = verticesToMove.ToArray<Vector3>();
+                upperObjectInstance.AddComponent<MeshCollider>();
+                upperObjectInstance.GetComponent<MeshCollider>().convex = true;
+
+
+                verticesToMove.Clear();
+                verticesIndexes.Clear();
+
+
+                lowerObjectInstance = Instantiate(correspondingLowerTrees[j], transform.position, transform.rotation, scrollCylinderParent.transform);
+                lowerObjectInstance.transform.localScale = transform.localScale;
+                lowerMesh = lowerObjectInstance.GetComponent<MeshFilter>().mesh;
+                verticesToMove = lowerMesh.vertices.ToList<Vector3>();
+
+                for (int i = 0; i < lowerMesh.vertices.Count(); i++)
+                {
+                    if (lowerMesh.vertices[i].z > height + Mathf.Tan(angle * (Mathf.PI / 180)) * verticesToMove[i].y)
+                    {
+                        verticesIndexes.Add(i);
+                    }
+                }
+
+                for (int i = 0; i < verticesIndexes.Count(); i++)
+                {
+                    verticesToMove[verticesIndexes[i]] = new Vector3(verticesToMove[verticesIndexes[i]].x, verticesToMove[verticesIndexes[i]].y, height + Mathf.Tan(angle * (Mathf.PI / 180)) * verticesToMove[verticesIndexes[i]].y);
+                }
+                lowerMesh.vertices = verticesToMove.ToArray<Vector3>();
+                lowerObjectInstance.AddComponent<MeshCollider>();
+                lowerObjectInstance.GetComponent<MeshCollider>().convex = true;
             }
         }
-
-        for (int i = 0; i < verticesIndexes.Count(); i++)
-        {
-            verticesToMove[verticesIndexes[i]] = new Vector3(verticesToMove[verticesIndexes[i]].x, verticesToMove[verticesIndexes[i]].y, height + Mathf.Tan(angle * (Mathf.PI / 180)) * verticesToMove[verticesIndexes[i]].y + slashWidth);
-        }
-        upperMesh.vertices = verticesToMove.ToArray<Vector3>();
-        upperObjectInstance.AddComponent<MeshCollider>();
-        upperObjectInstance.GetComponent<MeshCollider>().convex = true;
-
-
-        verticesToMove.Clear();
-        verticesIndexes.Clear();
-
-
-        lowerObjectInstance = Instantiate(lowerObjectToSpawn, transform.position, transform.rotation, scrollCylinderParent.transform);
-        lowerMesh = lowerObjectInstance.GetComponent<MeshFilter>().mesh;
-        verticesToMove = lowerMesh.vertices.ToList<Vector3>();
-
-        for (int i = 0; i < lowerMesh.vertices.Count(); i++)
-        {
-            if (lowerMesh.vertices[i].z > height + Mathf.Tan(angle * (Mathf.PI / 180)) * verticesToMove[i].y)
-            {
-                verticesIndexes.Add(i);
-            }
-        }
-
-        for (int i = 0; i < verticesIndexes.Count(); i++)
-        {
-            verticesToMove[verticesIndexes[i]] = new Vector3(verticesToMove[verticesIndexes[i]].x, verticesToMove[verticesIndexes[i]].y, height + Mathf.Tan(angle * (Mathf.PI / 180)) * verticesToMove[verticesIndexes[i]].y);
-        }
-        lowerMesh.vertices = verticesToMove.ToArray<Vector3>();
-        lowerObjectInstance.AddComponent<MeshCollider>();
-        lowerObjectInstance.GetComponent<MeshCollider>().convex = true;
 
         Destroy(gameObject);
     }
