15a1d61d46e4ee8deab717dc871e34ca1a88852b
ETU\a.coteyangting <s-a-f-e-r@outlook.fr>
2019-03-07 16:01:31 +0100

    improved axe trail + destroy cutted trees when they become unvisible

---
+++ Assets/Scripts/AxeTrailManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeTrailManager : MonoBehaviour
+{
+
+    public ParticleSystem trailFx;
+    Transform trailParent;
+    Vector3 initialPosition;
+    Quaternion initialRotation;
+    Vector3 initialScale;
+
+    private void Start()
+    {
+        trailFx.Stop();
+        trailParent = trailFx.gameObject.transform.parent;
+        initialPosition = trailFx.transform.position;
+        initialRotation = trailFx.transform.rotation;
+        initialScale = trailFx.transform.localScale;
+    }
+
+    private void Update()
+    {
+        print(initialPosition);
+        print(initialRotation);
+        print(initialScale);
+    }
+
+    void StartTrail()
+    {
+        trailFx.Play();
+        trailFx.transform.SetParent(trailParent);
+        trailFx.transform.position = initialPosition;
+        trailFx.transform.rotation = initialRotation;
+        trailFx.transform.localScale = initialScale;
+    }
+
+    void EndTrail()
+    {
+        trailFx.Stop();
+        trailFx.transform.SetParent(gameObject.transform);
+    }
+}
